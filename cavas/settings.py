33f83c5f70ffc87ba1d1db080858b098f8bd5828
RohitChauhan916 <rohitchauhan916@gmail.com>
2020-08-19 17:05:04 +0530

    -

--- cavas/settings.py
+++ cavas/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@=ldq$3@$_xbm3bllefrqfmo=&s20x1wk!=9!u2o*7x#-t03i%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cavas-app.herokuapp.com', '127.0.0.1']
 
